55eae561f299ff67ef4f1b044c0f25f3a360ded1
Missael Ortiz <ortiz.missael80@gmail.com>
2021-07-28 15:34:29 -0700

    initialized starting code

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,8 +2,9 @@ import pool from '../lib/utils/pool.js';
 import setup from '../data/setup.js';
 import request from 'supertest';
 import app from '../lib/app.js';
+import Reading from '../lib/models/Reading.js';
 
-describe('demo routes', () => {
+describe('demo reading routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
